9b76eeed8aa0c90a5db119e1f169f88140f4093f
Amel252 <amelghanem25@gmail.com>
2025-06-05 17:41:43 +0200

    installation react-router-dom / Création des routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,3 +1,23 @@
-function App() {}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/sign-in" element={<Signin />} />
+                <Route path="/sign-up" element={<Signup />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
 
 export default App;
