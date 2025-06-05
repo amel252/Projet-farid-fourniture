6296ad4dc587bdacd6c9a4cd5d732fcfe1013dad
Amel252 <amelghanem25@gmail.com>
2025-06-05 20:02:05 +0200

    installation mongoose dans le site

--- back-end/index.js
+++ back-end/index.js
@@ -1,7 +1,17 @@
 import express from "express"; // tu importe le framework express
+import mongoose from "mongoose";
+// dotenv on l'appelle avec ca
+dotenv.config();
 
 const app = express(); // Tu initialises une application Express. cette variable app qui te permet de définir des routes (GET, POST, etc.), ajouter du middleware,
-
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log("Connected to BD");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 app.listen(3000, () => {
     // Tu lances le serveur sur le port 3000 , listen démarre le serveur HTTP
     console.log("le serveur est écouté sur le port 3000");
