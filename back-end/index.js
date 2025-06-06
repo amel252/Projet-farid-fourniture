a5ec4f8d2d115482c221c707d02cd931802181b4
Amel252 <amelghanem25@gmail.com>
2025-06-07 00:21:36 +0200

    Création de l'API test avec user.controller et user.route

--- back-end/index.js
+++ back-end/index.js
@@ -3,6 +3,8 @@ import express from "express";
 import mongoose from "mongoose";
 // dotenv on l'appelle avec ca
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route";
+
 dotenv.config();
 
 const app = express(); // Tu initialises une application Express. cette variable app qui te permet de définir des routes (GET, POST, etc.), ajouter du middleware,
@@ -21,3 +23,5 @@ app.listen(3000, () => {
     // Tu lances le serveur sur le port 3000 , listen démarre le serveur HTTP
     console.log("le serveur est écouté sur le port 3000");
 });
+// import des routes
+app.use("/api/user", userRouter);
